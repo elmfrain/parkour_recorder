ab0145ce16b389db2972b307b3cf9e12ad132e0c
elmfrain <elmfer10@gmail.com>
2020-06-25 19:23:45 -0500

    initial update to 1.16.1

--- README.md
+++
@@ -1,2 +0,0 @@
-# parkourhelper
-A Minecraft mod that allows you to record and playback parkour sessions.
--- src/main/java/com/elmfer/parkourhelper/ParkourHelperMod.java
+++ src/main/java/com/elmfer/parkourhelper/ParkourHelperMod.java
@@ -3,12 +3,6 @@ package com.elmfer.parkourhelper;
 import com.elmfer.parkourhelper.proxies.CommonProxy;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = ParkourHelperMod.MOD_ID, name = ParkourHelperMod.MOD_NAME, version = ParkourHelperMod.MOD_VERSION)
 public class ParkourHelperMod 
@@ -16,7 +10,7 @@ public class ParkourHelperMod
 	
 	public static final String MOD_ID = "parkourhelper";
 	public static final String MOD_NAME = "Parkour Helper Mod";
-	public static final String MOD_VERSION = "0.0.4.6-1.12.2";
+	public static final String MOD_VERSION = "0.0.4.6-1.16.1";
 	
 	@SidedProxy(clientSide = "com.elmfer.parkourhelper.proxies.ClientProxy", serverSide = "com.elmfer.parkourhelper.proxies.ServerProxy")
 	public static CommonProxy proxy;
