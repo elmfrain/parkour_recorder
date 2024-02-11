960e7a22deb24a7f5b6691fe33cf8eff13cada88
elmfrain <elmfer10@gmail.com>
2024-02-11 03:30:58 -0600

    update mod version

--- src/main/java/com/elmfer/prmod/ParkourRecorder.java
+++ src/main/java/com/elmfer/prmod/ParkourRecorder.java
@@ -9,7 +9,7 @@ public class ParkourRecorder implements ClientModInitializer {
 
 	public static final String MOD_NAME = "Parkour Recorder";
 	public static final String MOD_ID = "prmod";
-	public static final String MOD_VERSION = "0.3.0a-mc1.20.4";
+	public static final String MOD_VERSION = "1.1.2.1-1.20.4";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
 	@Override
