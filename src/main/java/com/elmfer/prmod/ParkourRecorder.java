5172bb97eb424461e96cd5cc6f26a1034e627526
elmfrain <elmfer10@gmail.com>
2024-02-20 05:29:00 -0600

    execute workflow only on push

--- src/main/java/com/elmfer/prmod/ParkourRecorder.java
+++ src/main/java/com/elmfer/prmod/ParkourRecorder.java
@@ -9,7 +9,7 @@ public class ParkourRecorder implements ClientModInitializer {
 
     public static final String MOD_NAME = "Parkour Recorder";
     public static final String MOD_ID = "prmod";
-    public static final String MOD_VERSION = "1.1.2.1-1.20.4";
+    public static final String MOD_VERSION = "1.1.3.0-1.20.4";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
     @Override
